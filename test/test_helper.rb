f95c39b56314bb0199f85f23a9e81f13c1758336
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2010-01-04 21:28:23 -0200

    Make sure tests can run without specific environment.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environment")
+#require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environment")
+require File.expand_path(File.dirname(__FILE__) + "/rails_app/config/environment")
 require 'test_help'
 
 require 'rubygems'
