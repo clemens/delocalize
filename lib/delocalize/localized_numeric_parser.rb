9bfeef7118a35e19724e824ede598f5a0a70002b
Clemens Kofler <clemens@railway.at>
2010-01-17 23:14:16 +0100

    small refactoring

--- lib/delocalize/localized_numeric_parser.rb
+++ lib/delocalize/localized_numeric_parser.rb
@@ -3,14 +3,12 @@
 module Delocalize
   class LocalizedNumericParser
     class << self
-      # Parse numbers removing unneeded characters and replacing separator
-      # through I18n. The return will be a valid ruby Numeric value (as string).
+      # Parse numbers removing unneeded characters and replacing decimal separator
+      # through I18n. This will return a valid Ruby Numeric value (as String).
       def parse(value)
         if value.is_a?(String)
-          value     = value.dup
           separator = I18n.t(:'number.format.separator')
-          value.gsub!(/[^0-9\-#{separator}]/, '')
-          value.gsub!(separator, '.')
+          value = value.gsub(/[^0-9\-#{separator}]/, '').gsub(separator, '.')
         end
         value
       end
