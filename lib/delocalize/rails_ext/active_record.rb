683863ea8bc3887106dbd972d8fc9e113957d3f3
Clemens Kofler <clemens@railway.at>
2011-10-03 12:36:17 -0500

    proper rails 3 compatibility

--- lib/delocalize/rails_ext/active_record.rb
+++ lib/delocalize/rails_ext/active_record.rb
@@ -32,20 +32,6 @@ ActiveRecord::Base.class_eval do
   end
   alias_method_chain :write_attribute, :localization
 
-  # ugh
-  def self.define_write_method_for_time_zone_conversion(attr_name)
-    method_body = <<-EOV
-      def #{attr_name}=(time)
-        unless time.acts_like?(:time)
-          time = time.is_a?(String) ? (I18n.delocalization_enabled? ? Time.zone.parse_localized(time) : Time.zone.parse(time)) : time.to_time rescue time
-        end
-        time = time.in_time_zone rescue nil if time
-        write_attribute(:#{attr_name}, time)
-      end
-    EOV
-    evaluate_attribute_method attr_name, method_body, "#{attr_name}="
-  end
-
   def convert_number_column_value_with_localization(value)
     value = convert_number_column_value_without_localization(value)
     value = Numeric.parse_localized(value) if I18n.delocalization_enabled?
@@ -69,5 +55,26 @@ ActiveRecord::Base.class_eval do
     end
 
     old != value
+  end
+end
+
+ActiveRecord::Base.instance_eval do
+  def define_method_attribute=(attr_name)
+    if create_time_zone_conversion_attribute?(attr_name, columns_hash[attr_name])
+      method_body, line = <<-EOV, __LINE__ + 1
+        def #{attr_name}=(original_time)
+          time = original_time
+          unless time.acts_like?(:time)
+            time = time.is_a?(String) ? (I18n.delocalization_enabled? ? Time.zone.parse_localized(time) : Time.zone.parse(time)) : time.to_time rescue time
+          end
+          time = time.in_time_zone rescue nil if time
+          write_attribute(:#{attr_name}, original_time)
+          @attributes_cache["#{attr_name}"] = time
+        end
+      EOV
+      generated_attribute_methods.module_eval(method_body, __FILE__, line)
+    else
+      super
+    end
   end
 end
