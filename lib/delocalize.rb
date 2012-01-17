f01e5580c438b79e28378e03ec2a502a02dee9eb
Clemens Kofler <clemens@railway.at>
2012-01-17 23:20:36 +0100

    Add autoloads

--- lib/delocalize.rb
+++ lib/delocalize.rb
@@ -7,3 +7,9 @@ if defined?(Rails::Railtie)
 elsif defined?(Rails::Initializer)
   raise "This version of delocalize is only compatible with Rails 3.0 or newer"
 end
+
+module Delocalize
+  autoload :Delocalizable,           'delocalize/delocalizable'
+  autoload :LocalizedDateTimeParser, 'delocalize/localized_date_time_parser'
+  autoload :LocalizedNumericParser,  'delocalize/localized_numeric_parser'
+end
