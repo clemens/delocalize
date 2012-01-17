c6a9a45951a688b252fe53d9f1aa2c24f1770d39
Clemens Kofler <clemens@railway.at>
2012-01-17 22:17:31 +0100

    Actually, let's specify the conversions, too

--- lib/delocalize/delocalizable.rb
+++ lib/delocalize/delocalizable.rb
@@ -6,17 +6,45 @@ module Delocalize
 
     included do
       class_attribute :delocalizable_fields
+      class_attribute :delocalize_conversions
     end
 
     module ClassMethods
-      def delocalize(*fields)
+      def delocalize(conversions = {})
+        self.delocalize_conversions ||= {}
         self.delocalizable_fields ||= []
-        self.delocalizable_fields += fields.map(&:to_sym)
-        self.delocalizable_fields.uniq!
+
+        conversions.each do |field, type|
+          delocalizable_fields << field.to_sym unless delocalizable_fields.include?(field.to_sym)
+          delocalize_conversions[field.to_sym] = type.to_sym
+        end
+      end
+
+      def delocalizing?
+        delocalizable_fields.any?
       end
 
       def delocalizes?(field)
-        delocalizable_fields.include?(field.to_sym)
+        delocalizing? && (delocalizable_fields || []).include?(field.to_sym)
+      end
+
+      def delocalizes_type_for(field)
+        delocalize_conversions[field.to_sym]
+      end
+    end
+
+    # The instance methods are just here for convenience. They all delegate to their class.
+    module InstanceMethods
+      def delocalizing?
+        self.class.delocalizing?
+      end
+
+      def delocalizes?(field)
+        self.class.delocalizes?(field)
+      end
+
+      def delocalizes_type_for(field)
+        self.class.delocalizes_type_for(field)
       end
     end
   end
--- test/delocalizable_test.rb
+++ test/delocalizable_test.rb
@@ -9,36 +9,94 @@ end
 class DelocalizableTest < ActiveSupport::TestCase
   setup do
     DelocalizableClass.delocalizable_fields = []
+    DelocalizableClass.delocalize_conversions = {}
+  end
+
+  test "tells that it is delocalizing" do
+    DelocalizableClass.delocalize :foo => :number, :bar => :time
+    assert DelocalizableClass.delocalizing?
+  end
+
+  test "tells that it is not delocalizing" do
+    assert !DelocalizableClass.delocalizing?
+  end
+
+  test "tells that it is delocalizing (on an instance)" do
+    DelocalizableClass.delocalize :foo => :number, :bar => :time
+    assert DelocalizableClass.new.delocalizing?
+  end
+
+  test "tells that it is not delocalizing (on an instance)" do
+    assert !DelocalizableClass.new.delocalizing?
   end
 
   test "stores the delocalizable fields" do
-    DelocalizableClass.delocalize :foo, :bar
+    DelocalizableClass.delocalize :foo => :number, :bar => :time
     assert_equal [:foo, :bar], DelocalizableClass.delocalizable_fields
   end
 
   test "stores the delocalizable fields as symbols" do
-    DelocalizableClass.delocalize 'foo', 'bar'
+    DelocalizableClass.delocalize 'foo' => 'number', 'bar' => 'time'
     assert_equal [:foo, :bar], DelocalizableClass.delocalizable_fields
   end
 
   test "stores the delocalizable fields without overriding existing ones" do
-    DelocalizableClass.delocalize :foo, :bar
-    DelocalizableClass.delocalize :baz
+    DelocalizableClass.delocalize :foo => :number, :bar => :time
+    DelocalizableClass.delocalize :baz => :date
     assert_equal [:foo, :bar, :baz], DelocalizableClass.delocalizable_fields
   end
 
   test "stores the delocalizable fields without duplicates" do
-    DelocalizableClass.delocalize :foo, :foo, :bar
+    DelocalizableClass.delocalize :foo => :number, :bar => :time
+    DelocalizableClass.delocalize :foo => :date
     assert_equal [:foo, :bar], DelocalizableClass.delocalizable_fields
   end
 
+  test "stores conversions" do
+    DelocalizableClass.delocalize :foo => :number, :bar => :time
+    assert_equal :number, DelocalizableClass.delocalize_conversions[:foo]
+    assert_equal :time, DelocalizableClass.delocalize_conversions[:bar]
+  end
+
+  test "stores conversions as symbols" do
+    DelocalizableClass.delocalize 'foo' => 'number', 'bar' => 'time'
+    assert_equal :number, DelocalizableClass.delocalize_conversions[:foo]
+    assert_equal :time, DelocalizableClass.delocalize_conversions[:bar]
+  end
+
+  test "stores conversions and overrides previous settings" do
+    DelocalizableClass.delocalize :foo => :number
+    DelocalizableClass.delocalize :foo => :date
+    assert_equal :date, DelocalizableClass.delocalize_conversions[:foo]
+  end
+
   test "tells that a field is to be delocalized" do
-    DelocalizableClass.delocalize :foo, :bar
+    DelocalizableClass.delocalize :foo => :number
     assert DelocalizableClass.delocalizes?(:foo)
   end
 
   test "tells that a field is not to be delocalized" do
-    DelocalizableClass.delocalize :foo, :bar
+    DelocalizableClass.delocalize :foo => :number
     assert !DelocalizableClass.delocalizes?(:baz)
+  end
+
+  test "tells that a field is to be delocalized (on an instance)" do
+    DelocalizableClass.delocalize :foo => :number
+    assert DelocalizableClass.new.delocalizes?(:foo)
+  end
+
+  test "tells that a field is not to be delocalized (on an instance)" do
+    DelocalizableClass.delocalize :foo => :number
+    assert !DelocalizableClass.new.delocalizes?(:baz)
+  end
+
+  test "tells the delocalize type of a field" do
+    DelocalizableClass.delocalize :foo => :number
+    assert_equal :number, DelocalizableClass.delocalizes_type_for(:foo)
+  end
+
+  test "tells the delocalize type of a field (on an instance)" do
+    DelocalizableClass.delocalize :foo => :number
+    assert_equal :number, DelocalizableClass.new.delocalizes_type_for(:foo)
   end
 end
