bc0d0faf3ced8b394bfb4b549d5a34c7e63c660c
Clemens Kofler <clemens@railway.at>
2013-08-11 18:49:33 +0200

    Update railtie

--- lib/delocalize/railtie.rb
+++ lib/delocalize/railtie.rb
@@ -2,8 +2,12 @@ module Delocalize
   class Railtie < Rails::Railtie
     initializer "delocalize" do |app|
       ActiveSupport.on_load :action_controller do
+        # By default, we use ActionController::Parameters provided by strong_parameters. If it's
+        # not there, we fall back to our own replacement (Delocalize::Parameters).
         if defined?(ActionController::Parameters)
           ActionController::Parameters.send(:include, Delocalize::ParameterDelocalizing)
+        else
+          ActionController::Base.send(:include, Delocalize::DelocalizableParameters)
         end
       end
     end
